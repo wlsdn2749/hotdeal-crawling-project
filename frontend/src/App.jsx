af22819bea11eaa61bbc2aceedf8d144c4482727
root <root@DESKTOP-ITGGK6I>
2024-06-10 00:44:58 +0900

    feat: wsl 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      리눅스만의 장점이라고 한다면?
+      ? 프론트에서는 뭐가 장점인데
+      뭐?
+      ?
+      ㅋㅋㅋㅋ
+      일단 깔았잖아 한잔해
+      그건 맞네
+
     </>
   )
 }
