cc0067a4beb73d7259fce6c632804ad9e4e2bb50
root <root@DESKTOP-ITGGK6I>
2024-06-13 17:36:56 +0900

    update: 세부정보 더미데이터 삭제, 서버 연동

--- frontend/src/components/Detail.jsx
+++ frontend/src/components/Detail.jsx
@@ -15,28 +15,9 @@ const Detail = () => {
             setLoading(true);
             try {
 
-                // const { item } = await detailItem(site, url);
-                // console.log('detail에서 가져온 item', item);
-                const item = {
-                    "title": "[G마켓] 2개XCGN 실리마린 컴플렉스 1년분 300mg 360베지캡슐(삼카) (49,060원) (무료)",
-                    "date": "2024.06.11 13:11",
-                    "author": "코코쟈니",
-                    "views": "9102",
-                    "likes": "14",
-                    "comment_count": "21",
-                    "related_url": "https://item.gmarket.co.kr/Item?goodscode=2904552108",
-                    "shoppingmall": "G마켓",
-                    "product_name": "2개XCGN 실리마린 컴플렉스 1년분 300mg 360베지캡슐(삼카)",
-                    "price": "49,060원",
-                    "deliveryfee": "무료",
-                    "article": [
-                        "유클쿠폰+20%쿠폰+삼카할인까지하면 49,060원으로 1통당 24,530원 (삼카없을경우는 25,030원)",
-                        "저번에 올라온거보다 싸길래 올림",
-                        "요즘은 종비+오메가에 거의 필수적으로 같이 먹는 영양제중에 하나인듯",
-                        "이거는 간세포 보호하고 재생하는데에 도움주기때문에 자기전에 복용하는게 좋다고 본거같네"
-                    ],
-                    "comments": "코멘트"
-                }
+                const { item } = await detailItem(site, url);
+                console.log('detail에서 가져온 item', item);
+
 
                 setItem(item);
                 setLoading(false);
@@ -97,10 +78,11 @@ const Detail = () => {
                     </tbody>
                 </table>
                 <div className="detail-article">
-                    {item.article.map((paragraph, index) => (
+                    {(item.article ?? []).map((paragraph, index) => (
                         <p key={index}>{paragraph}</p>
                     ))}
                 </div>
+
                 <div className="detail-comments">
                     <h2>댓글</h2>
                     <p>{item.comments}</p>
