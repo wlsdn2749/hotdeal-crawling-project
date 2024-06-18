1b743815bbde02a7f9c82b318375ec870718f8d9
Serotina <wlsdn2749@gmail.com>
2024-06-18 14:33:02 +0900

    Update: Categories Integreted 2

--- api/utils.py
+++
@@ -1,6 +0,0 @@
-from typing import Optional, List
-
-def load_valid_categories(file_path: str) -> set:
-    with open(file_path, 'r', encoding="utf-8") as f:
-        categories = {line.strip() for line in f if line.strip()}
-    return categories
--- hotdeal/database.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('hotdeal.db')
-
-c = conn.cursor()
-c.execute("Create table hotdeal ")
